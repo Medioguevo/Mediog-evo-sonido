5293c8d71a0dd576551a0dc12e228459aff2ea4d
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2022-10-01 13:03:55 +0200

    Fixes path

---
+++ src/app_config.js
@@ -0,0 +1,8 @@
+export default {
+    name: "Mediogüevo efectos de sonido",
+    shortName: "Mediogüevo",
+    description: "Controlar efectos de sonido con el teléfono para grupos de teatro sin pretensiones.",
+    startUrl: ".",
+    backgroundColor: "#212529",
+    themeColor: "yellow",
+}
---
+++ src/routes/manifest.json/+server.ts
@@ -0,0 +1,49 @@
+import type { RequestHandler } from "@sveltejs/kit";
+import app_config from "src/app_config"
+
+export const prerender = true;
+
+export const GET: RequestHandler = function get({ setHeaders }) {
+
+  const { backgroundColor, description, shortName, name, themeColor } = app_config;
+
+  const manifest = {
+    $schema: "https://json.schemastore.org/web-manifest-combined.json",
+    name: name,
+    short_name: shortName,
+    description: description,
+    start_url: ".",
+    background_color: backgroundColor,
+    theme_color: themeColor,
+    display: 'standalone',
+    icons: [
+      {
+        "src": "images/icon.svg",
+        "type": "image/svg+xml",
+        "sizes": "144x144",
+      },
+      {
+        "src": "images/splash-icon.png",
+        "type": "image/png",
+        "sizes": "512x512",
+      },
+      {
+        "src": "images/icon.svg",
+        "sizes": "192x192",
+        "type": "image/svg+xml",
+        "purpose": "maskable"
+      },
+    ],
+/*
+    related_applications: [{
+      "platform": "play",
+      "url": "https://play.google.com/store/apps/details?id=cheeaun.hackerweb"
+    }]
+*/
+  }
+	setHeaders({
+		'content-type': 'application/json',
+	});
+	return new Response(JSON.stringify(manifest));
+};
+
