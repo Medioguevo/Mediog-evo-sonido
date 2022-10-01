b6b888ba542b0d37f898263f2da92f454c1319bf
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2022-10-01 17:16:44 +0200

    Set simpler onFetch

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -11,6 +11,18 @@ const RUNTIME_CACHE = 'runtime';
 
 const FILES_FOR_INSTALLATION_CACHE = build.concat(files);
 
+function showCacheContent() {
+	caches.open(INSTALLATION_CACHE).then(function(cache) {
+		cache.keys().then(function(requests) {
+		var urls = requests.map(function(request) {
+			return request.url;
+		});
+		console.log(urls.sort());
+		});
+	});
+}
+
+
 worker.addEventListener('install', (event: ExtendableEvent) => {
 	event.waitUntil(
 		caches.open(INSTALLATION_CACHE)
@@ -34,6 +46,7 @@ worker.addEventListener('activate', (event: ExtendableEvent) => {
 });
 
 worker.addEventListener('fetch', (event: ExtendableEvent) => {
+/*	showCacheContent()
 	if (event.request.url.startsWith(self.location.origin)) {
 		event.respondWith(
 		  caches.match(event.request)
@@ -45,4 +58,5 @@ worker.addEventListener('fetch', (event: ExtendableEvent) => {
 			)
 		)
 	}
+	*/
 });
