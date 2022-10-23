d6464de5d6c77cbd82331a7404da0d66ba02ef1d
Daniel Banore Dopico <daniel.banobre.dopico@pm.me>
2022-10-23 18:07:54 +0200

    Group configurations in a single site

--- src/lib/defines.ts
+++
@@ -1,2 +0,0 @@
-export const MIDDLE_VOLUME = 0.2
-export const FADE_TIME = 1000
--- src/routes/play-[play]/volumeControl.ts
+++ src/routes/play-[play]/volumeControl.ts
@@ -1,10 +1,9 @@
 import { writable, type Writable } from 'svelte/store'
 import { tweened } from 'svelte/motion'
 import type {Volume} from '$lib/types'
+import app_config from '../../app_config'
 
-import {FADE_TIME, MIDDLE_VOLUME} from '$lib/defines'
-
-export let volumeControl = writable ({
-    currentVolume: tweened(1, {duration: FADE_TIME}),
-    middleVolume: MIDDLE_VOLUME,
+export let volumeControl: Writable<Volume> = writable ({
+    currentVolume: tweened(1, {duration: app_config.fadeTime}),
+    middleVolume: app_config.middleVolume,
 })
